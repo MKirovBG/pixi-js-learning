b0fe772b3eb7173bfcdb7bb8d51a122cfc1d91ad
MK <68346322+MKirovBG@users.noreply.github.com>
2022-11-21 15:15:51 +0000

    add Character & movement

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let app;
 let player;
+let keys = {};
+let keysDiv;
 
 window.onload = function () {
 	//Create the application
@@ -25,16 +27,59 @@ window.onload = function () {
 	app.stage.addChild(player);
 
 	//interaction - Move with mouse
-	app.stage.interactive = true;
-	// stage the changes with function which is making the movement
-	// the stage takes two arguments (name, function)
-	app.stage.on('pointermove', movePlayer);
+	// app.stage.interactive = true;
+
+	/* stage the changes with function which is making the movement
+	 the stage takes two arguments (name, function)*/
+
+	// app.stage.on('pointermove', movePlayer);
+
+	// Keyboar event handler
+	window.addEventListener('keydown', keysDown);
+	window.addEventListener('keyup', keysUp);
+
+	//add Ticker
+
+	app.ticker.add(gameLoop);
+
+	keysDiv = document.querySelector('#keys');
 };
 
+// w s a d
+// 87 83 65 68
+
+function keysDown(e) {
+	keys[e.keyCode] = true;
+}
+
+function keysUp(e) {
+	keys[e.keyCode] = false;
+}
+
+function gameLoop(e) {
+	keysDiv.innerHTML = JSON.stringify(keys);
+
+	if (keys['87']) {
+		player.y -= 5;
+	}
+	if (keys['83']) {
+		player.y += 5;
+	}
+	if (keys['65']) {
+		player.x -= 5;
+	}
+	if (keys['68']) {
+		player.x += 5;
+	}
+}
+
 // follow mouse function on hover
+
+/*
 function movePlayer(e) {
 	let position = e.data.global;
 
 	player.x = position.x;
 	player.y = position.y;
 }
+*/
