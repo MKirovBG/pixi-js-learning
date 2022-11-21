89e9ad7e08d2aed112422f1f0262bc1da86a96df
MK <68346322+MKirovBG@users.noreply.github.com>
2022-11-21 13:37:08 +0000

    Changing the default configuration

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
-const Application = PIXI.Application; // class to create a new PixiJS application.
+let app;
+let player;
 
-const app = new Application({
-	width: 800, // give width
-	height: 600, // give hight
-	transparent: false, // transparency 0
-	antialias: true, // antialias
-});
+window.onload = function () {
+	//Create the application
+	app = new PIXI.Application({
+		//  set values as object with CSS properties
+		width: 600,
+		height: 600,
+		backgroundColor: 0xaaaaaa,
+	});
 
-app.renderer.backgroundColor = 0x23395d; // change the background
+	//add the app to the DOM
+	document.body.appendChild(app.view);
 
-app.renderer.resize(window.innerWidth, window.innerHeight); // give the app max width and hight
+	// add image to the application
+	player = PIXI.Sprite.from('./images/cat.png');
 
-app.renderer.view.style.position = 'absolute'; // position the app absolute
+	// position the image
+	player.anchor.set(0.5);
+	player.x = app.view.width / 2;
+	player.y = app.view.height / 2;
 
-document.body.appendChild(app.view); // update the DOM
+	// stage the image to the APP / update DOM
+	app.stage.addChild(player);
 
-const Graphics = PIXI.Graphics; // PIXI.Graphics is a draw method
+	//interaction - Move with mouse
+	app.stage.interactive = true;
+	// stage the changes with function which is making the movement
+	// the stage takes two arguments (name, function)
+	app.stage.on('pointermove', movePlayer);
+};
 
-/*
-The Graphics class is primarily used to render primitive shapes such as lines, circles and rectangles to the display, and to color and fill them. However, you can also use a Graphics object to build a list of primitives to use as a mask, or as a complex hitArea.
-*/
+// follow mouse function on hover
+function movePlayer(e) {
+	let position = e.data.global;
 
-const rectangle = new Graphics();
-
-rectangle
-	.beginFill(0xaaa33bb)
-	.lineStyle(4, 0xffa00, 1)
-	.drawRect(200, 200, 100, 120)
-	.endFill();
-
-app.stage.addChild(rectangle);
+	player.x = position.x;
+	player.y = position.y;
+}
